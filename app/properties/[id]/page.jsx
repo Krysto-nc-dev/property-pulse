f82f96c770252adc39df862ffdb732ec5e6d2d8d
Velten Stoyann <krysto.dev@gmail.com>
2024-07-26 18:31:30 +1100

    fetch single property

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,11 +1,49 @@
-'use client'
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { fetchProperty } from '@/utils/requests';
 
 const PropertyPage = () => {
+  const { id } = useParams();
+
+  const [property, setProperty] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if (!id) return;
+      try {
+        const property = await fetchProperty(id);
+        setProperty(property);
+      } catch (error) {
+        console.error('Impossible de récupérer la propriété', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPropertyData();
+  }, [id]);
+
+  if (loading) {
+    return <p>Chargement de la propriété...</p>;
+  }
+
+  if (!property) {
+    return <p>Aucune propriété trouvée</p>;
+  }
+
   return (
     <div>
       <h1 className="text-3xl">Propriété</h1>
+      <div>
+        <h2 className="text-xl">{property.name}</h2>
+        <p>{property.description}</p>
+        {/* Ajoutez d'autres champs de propriété ici */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PropertyPage
+export default PropertyPage;
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null
 
+// fetch all properties
 async function fetchProperties() {
   try {
     // handle the case where the domain is not available yet
@@ -19,4 +20,24 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties }
+// Fetch single property
+
+async function fetchProperty(id) {
+  try {
+    if (!apiDomain) {
+      return null
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`)
+    if (!res.ok) {
+      throw new Error(
+        `Erreur lors de la récupération de la propriété : ${res.status}`,
+      )
+    }
+    return res.json()
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
+export { fetchProperties, fetchProperty }
