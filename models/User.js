697765899a9062ba371b03f914d85b94b6523b7d
Velten Stoyann <krysto.dev@gmail.com>
2024-07-26 15:34:11 +1100

    property and user models

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import { Schema, model, models } from 'mongoose'
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, 'Cette email existe déjà'],
+      required: [true, 'Le mail est requis'],
+    },
+    username: {
+      type: String,
+      required: [true, "Le nom d'utilisateur est requis"],
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Property',
+      },
+    ],
+  },
+  { timestamps: true },
+)
+
+const User = models.User || model('User', UserSchema)
+
+export default User
