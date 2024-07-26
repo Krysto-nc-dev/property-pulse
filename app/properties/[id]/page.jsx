698095bb3dc8db16cecb118c41d11448058b77ab
Velten Stoyann <krysto.dev@gmail.com>
2024-07-27 09:50:07 +1100

    property details component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'next/navigation';
 import { fetchProperty } from '@/utils/requests';
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import { FaArrowLeft } from 'react-icons/fa';
+import PropertyDetails from '@/components/PropertyDetails';
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -60,75 +61,7 @@ const PropertyPage = () => {
           <section className="bg-blue-50">
             <div className="container m-auto py-10 px-6">
               <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-                <main>
-                  <div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-                    <div className="text-gray-500 mb-4">Appartement</div>
-                    <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
-                    <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-                      <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
-                      <p className="text-orange-700">
-                        {property.location.street} {property.location.city}, {property.location.state} {property.location.zipcode}
-                      </p>
-                    </div>
-
-                    <h3 className="text-lg font-bold my-6 bg-gray-800 text-white p-2">
-                      Tarifs et Options
-                    </h3>
-                    <div className="flex flex-col md:flex-row justify-around">
-                      <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-                        <div className="text-gray-500 mr-2 font-bold">Nuitée</div>
-                        <div className="text-2xl font-bold">
-                          <i className="fa fa-xmark text-red-700"></i>
-                        </div>
-                      </div>
-                      <div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-                        <div className="text-gray-500 mr-2 font-bold">Semaine</div>
-                        <div className="text-2xl font-bold text-blue-500">{property.rates.weekly} XPF</div>
-                      </div>
-                      <div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
-                        <div className="text-gray-500 mr-2 font-bold">Mois</div>
-                        <div className="text-2xl font-bold text-blue-500">{property.rates.monthly} XPF</div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                    <h3 className="text-lg font-bold mb-6">Description et Détails</h3>
-                    <div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
-                      <p>
-                        
-                        <i className="fa-solid fa-bed"></i> {property.beds}
-                        <span className="hidden sm:inline"> Lits</span>
-                      </p>
-                      <p>
-                        <i className="fa-solid fa-bath"></i> {property.baths}
-                        <span className="hidden sm:inline"> Salles de bain</span>
-                      </p>
-                      <p>
-                        <i className="fa-solid fa-ruler-combined"></i>
-                        {property.square_feet} <span className="hidden sm:inline">m²</span>
-                      </p>
-                    </div>
-                    <p className="text-gray-500 mb-4">
-                      {property.description}
-                    </p>
-                  </div>
-
-                  <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                    <h3 className="text-lg font-bold mb-6">Équipements</h3>
-
-                    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
-                      {property.amenities.map((amenity, index) => (
-                        <li key={index}>
-                          <i className="fas fa-check text-green-600 mr-2 mt-3"></i> {amenity}
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                    <div id="map"></div>
-                  </div>
-                </main>
+              <PropertyDetails property={property}/>
 
                 {/* Sidebar */}
                 <aside className="space-y-4">
